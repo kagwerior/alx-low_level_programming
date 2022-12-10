42967e36d7f4723d1ba24748d991f2a5c6947984
kagwerior <kagwerior@gmail.com>
2022-12-09 16:46:19 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets both lower cases and upper cases
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 9; number++)
+	{
+		putchar(number + '0');
+		if (number < 9)
+		{
+			putchar('.');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
