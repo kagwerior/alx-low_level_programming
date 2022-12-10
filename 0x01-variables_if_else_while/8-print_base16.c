aeb38e3b4227e86ebd7278743a10f4dd904fca33
kagwerior <kagwerior@gmail.com>
2022-12-09 16:25:02 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets both lower cases and upper cases
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	int num;
+
+	for (num = 0; number < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
